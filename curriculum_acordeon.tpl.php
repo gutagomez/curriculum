febcf4ec359680d4b04d6c2b4cb1608558a2fcf7
Gustavo Gómez <gutagomez@guta-Mini.(none)>
2012-11-02 10:53:11 +0100

    curriculum 1.0

---
+++ curriculum_acordeon.tpl.php
@@ -0,0 +1,144 @@
+<?php
+
+/**
+ * @file
+ * Template file for js_example module.
+ */
+?>
+
+<div class="demo">
+<h2><?php print $title; ?></h2>
+<div id="accordion">
+  <h3><a href="#">Datos presonales</a></h3>
+	<div>
+			<div><h1>Gustavo Gómez Campo</h1></div>
+			<div>c/ Casto Sampedro nº 4-2º<br>
+			Redondela, 36800 Pontevedra<br>
+			Nacionalidad: Española<br>
+			Fecha Nacimiento: 21-12-1976<br>
+			Teléfono fijo: 986400535<br>
+			Teléfono móvil: 617419697<br>
+			e-mail: gutagomez@gmail.com<br>
+			Twitter: @gutagomez<br>
+			Skype: gutagomez<br>
+			</div>
+	</div>
+
+  <h3><a href="#">Perfil profesional</a></h3>
+  <div>
+	<p>Desarrollador y maquetador en Drupal 6 y 7, con capacidades y esperiencia durante 3 años en
+desarrollar tanto sitios web corporativos como ecommerce desde 0 con drupal de forma freelance,
+desde el apartado conceptual y boceto hasta su final implantación y seguimiento. Algunos de mis
+últimos trabajos son www.gutapc.com , www.eguta.com , www.gruporodriguezcounago.es , como
+freelance me veo en la obligación de realizar por mi mismo todas las partes del desarrollo de un
+proyecto web por lo que también tengo conocimientos en otras disciplinas como SEO, Analítica
+Web, Usabilidad. A pesar de trabajar muchas veces sólo, prefiero el trabajo en equipo. Me gusta
+buscar, adaptarme y aprender nuevas tecnologías o herramientas que surjan para mejorar mi trabajo
+y productividad.
+	</p>
+  </div>	
+  <h3><a href="#">Habilidades</a></h3>
+  <div>
+	
+		<div><b>CSS: </b>Amplio conocimiento de CSS y CSS3. Intento respetar estandares, que los diseño sean lo más
+compatible posible con los navegadores y que mis sitios sean responsive design.<br><b>Experiencia: </b> 3 años de experiencia en el manejo de CSS y 1 año de experiencia con CSS3
+		</div>
+		<div><b>Linux: </b>Amplio conocimiento de sistemas linux, así como de administrador de redes bajo linux.<br><b>Experiencia: </b> 5 años de experiencia.
+		</div>
+		<div><b>SEO: </b>En mis proyectos con DRUPAL, empleo módulos que ayudan al SEO y a la indexación por los buscadores. Tengo conocimientos de las principales técnicas de localización de palabras claves adecuadas a cada proyecto y de como emplearlas en los sitios web para su optimización SEO.<br><b>Experiencia: </b> 1 años empleando SEO en mis proyectos.
+		</div>
+		<div><b>Analítica Web: </b>Es otra técnica que empleo para hacer el seguimineto de mis proyectos y hacer una evaluación de lo que lo que debo mejorar en ellos.<br><b>Experiencia: </b>1 año empleando anaítica web para hacer seguimiento de mis proyectos.
+		</div>
+		<div><b>PHP: </b>Manejo PHP para poder desarrollar modulos en DRUPAL y modificar las partes de DRUPAL que necesito.<br><b>Experiencia: </b> 2 años de experiencia en el manejo de PHP.
+		</div>
+		<div><b>JAVASCRIPT: </b>Conocimientos de javascrip para complementar o modificar las partes que drupal necesita.<br><b>Experiencia: </b> 1 años de experiencia.
+		</div>
+		<div><b>GIT: </b>Desde que drupal.org escogió el sistema de versiones de GIT he empezado a usarlo.<br><b>Experiencia: </b>Medio año usando GIT como sistema de control de versiones.
+		</div>
+	
+  </div>
+  <h3><a href="#">Experiencia profesional</a></h3>
+  <div>
+	<div>
+		<div><b>2005-2008</b></div>
+		<div><b>Confitería filo, S.L.</b></div>
+		<div>Cargo:</div>
+		<div>Ayudante de pastelero</div>
+		<div>Función:</div>
+		<div>Ayudante de pastelero y repartidor en empresa familiar.</div>
+	</div>
+	<div>
+		<div><b>2008-2010</b></div>
+		<div><b>Comunicaciones Dacuña y Perez, S.L.</b></div>
+		<div>Cargo:</div>
+		<div>Técnico informático.</div>
+		<div>Función</div>
+		<div>Encargado del mantenimiento informático y del desarrollo y mantenimiento de la web de la empresa realizaa en drupal 6.</div>
+	</div>
+	<div>
+		<div><b>2010-hasta ahora</b></div>
+		<div><b>Trabajador por cuenta propia</b></div>
+		<div>Cargo:</div>
+		<div>Autónomo/freelance</div>
+		<div>Proyectos:</div>
+		<div>
+			<ul>
+				<li>www.gutapc.com Tienda online dedica a la venta de productos electrónicos realizada y mantenida por mi en drupal 6 desde el 2010 hasta ahora.</li>
+				<li>www.mcarquitectos.es Sitio web realizado por mi con drupal 6 durante el 2011 para un estudio de arquitectura.</li>
+				<li>www.eguta.com Página realizada por mi con drupal 7 desde la cual ofrezco mis servicios de diseño web desde el 2012 hasta ahora.</li>
+				<li>www.gruporodriguezcounago.es Sitio realizado por mi en drupal 7 durante el 2012 para un empresa constructora.</li>
+			</ul>			
+		</div>
+	</div>
+  </div>
+  <h3><a href="#">Formación profesional</a></h3>
+  <div>
+	<div>
+		<div><b>1994</b></div>
+		<div><b>PET</b></div>
+		<div>Examén de ingles de la universidad de cambridge realizado en pontevedra (nivel básico).</div>
+	</div>
+	<div>
+		<div><b>1994</b></div>
+		<div><b>Ciclo elemental (Inglés).</b></div>
+		<div>Escola oficial de idiomas de pontevedra.</div>
+	</div>
+	<div>
+		<div><b>1991-1995</b></div>
+		<div><b>Titulo bachiller.</b></div>
+		<div>IES mendiño. Redondela.</div>
+	</div>
+	<div>
+		<div><b>1996-1997</b></div>
+		<div><b>Estudios Geología.</b></div>
+		<div>Universidad de Oviedo.</div>
+	</div>
+	<div>
+		<div><b>1999-2004</b></div>
+		<div><b>Estudios Económicas.</b></div>
+		<div>Universidad de vigo.</div>
+	</div>
+	<div>
+		<div><b>2006-hasta ahora.</b></div>
+		<div><b>Ingeniería técnica informática de sistemas.</b></div>
+		<div>U.N.E.D<br>En curso, compaginandola con los distintos trabajos que tuve.</div>
+	</div>
+
+
+  </div>
+  <h3><a href="#">Otros datos</a></h3>
+  <div>
+	<div>
+		<ul>
+			<li>“Master en diseño web”. 2004</li>
+			<li>“Sair o mercado”. 2012 Curso realizado en la asociacion de jovenes empresarios de vigo, consistente en como llevar una empresa, definicion del plan estrategico, plan de marketing, recursos humanos etc</li>
+			<li>“Curso online de experto en Marketing electrónico para la internalización digital de la PYME” (UNED). 2012</li>
+			<li>Desde 1998 – 2010 compito como semiprofesional en distintos equipos de balonmano, con lo que conozco y estoy acostrumbrado a realizar trabajos en equipo.</li>
+
+		</ul>
+	</div>
+  </div>
+
+</div>
+
+</div><!-- End demo -->
---
+++ curriculum_css.tpl.php
@@ -0,0 +1,146 @@
+<div id="cabecera">
+		<div class="foto"></div>
+	<div class="datos_personales">
+		<div class="nombre"><h1>Gustavo Gómez Campo</h1></div>
+		<div class="datos">c/ Casto Sampedro nº 4-2º<br>
+		Redondela, 36800 Pontevedra<br>
+		Nacionalidad: Española<br>
+		Fecha Nacimiento: 21-12-1976<br>
+		Teléfono fijo: 986400535<br>
+		Teléfono móvil: 617419697<br>
+		e-mail: gutagomez@gmail.com<br>
+		Twitter: @gutagomez<br>
+		Skype: gutagomez<br>
+		</div>
+
+	</div>
+</div>
+
+<section class="ac-container">
+<div id="perfil_profesional">
+	<input id="ac-1" name="accordion-1" type="checkbox" />
+	<label for="ac-1"  class="titulo">Perfil Profesional</label>
+	<article class="ac-small" >
+	<b>Experto en drupal:</b>
+	<p>Desarrollador y maquetador en Drupal 6 y 7, con capacidades y esperiencia durante 3 años en
+desarrollar tanto sitios web corporativos como ecommerce desde 0 con drupal de forma freelance,
+desde el apartado conceptual y boceto hasta su final implantación y seguimiento. Algunos de mis
+últimos trabajos son www.gutapc.com , www.eguta.com , www.gruporodriguezcounago.es , como
+freelance me veo en la obligación de realizar por mi mismo todas las partes del desarrollo de un
+proyecto web por lo que también tengo conocimientos en otras disciplinas como SEO, Analítica
+Web, Usabilidad. A pesar de trabajar muchas veces sólo, prefiero el trabajo en equipo. Me gusta
+buscar, adaptarme y aprender nuevas tecnologías o herramientas que surjan para mejorar mi trabajo
+y productividad.
+	</p>
+	</article>
+</div>
+<div id="habilidades">
+	<input id="ac-2" name="accordion-1" type="checkbox" />
+	<label for="ac-2" class="titulo">Habilidades</label>
+	<article class="ac-large">
+		<div class="caracteristica"><b>Html: </b>Amplio conocimiento de HTML y HTML5<br><b>Experiencia: </b> 5 años de experiencia en el manejo de HTML
+		</div>
+		<div class="caracteristica"><b>CSS: </b>Amplio conocimiento de CSS y CSS3. Intento respetar estandares, que los diseño sean lo más
+compatible posible con los navegadores y que mis sitios sean responsive design.<br><b>Experiencia: </b> 3 años de experiencia en el manejo de CSS y 1 año de experiencia con CSS3
+		</div>
+		<div clas s="caracteristica"><b>Linux: </b>Amplio conocimiento de sistemas linux, así como de administrador de redes bajo linux.<br><b>Experiencia: </b> 5 años de experiencia.
+		</div>
+		<div class="caracteristica"><b>SEO: </b>En mis proyectos con DRUPAL, empleo módulos que ayudan al SEO y a la indexación por los buscadores. Tengo conocimientos de las principales técnicas de localización de palabras claves adecuadas a cada proyecto y de como emplearlas en los sitios web para su optimización SEO.<br><b>Experiencia: </b> 1 años empleando SEO en mis proyectos.
+		</div>
+		<div class="caracteristica"><b>Analítica Web: </b>Es otra técnica que empleo para hacer el seguimineto de mis proyectos y hacer una evaluación de lo que lo que debo mejorar en ellos.<br><b>Experiencia: </b>1 año empleando anaítica web para hacer seguimiento de mis proyectos.
+		</div>
+		<div class="caracteristica"><b>PHP: </b>Manejo PHP para poder desarrollar modulos en DRUPAL y modificar las partes de DRUPAL que necesito.<br><b>Experiencia: </b> 2 años de experiencia en el manejo de PHP.
+		</div>
+		<div class="caracteristica"><b>JAVASCRIPT: </b>Conocimientos de javascrip para complementar o modificar las partes que drupal necesita.<br><b>Experiencia: </b> 1 años de experiencia.
+		</div>
+		<div class="caracteristica"><b>GIT: </b>Desde que drupal.org escogió el sistema de versiones de GIT he empezado a usarlo.<br><b>Experiencia: </b>Medio año usando GIT como sistema de control de versiones.
+		</div>
+	</article>
+</div>
+<div id="experiencia_profesional">
+	<input id="ac-3" name="accordion-1" type="checkbox" />
+	<label for="ac-3" class="titulo">Experiencia profesional</label>
+	<article class="ac-large">
+	<div class="experiencia">
+		<div class="ano"><b>2005-2008</b></div>
+		<div class="empresa"><b>Confitería filo, S.L.</b></div>
+		<div class="cargo">Cargo:</div>
+		<div class="datos_cargo">Ayudante de pastelero</div>
+		<div class="proyectos">Función:</div>
+		<div class="datos_proyectos">Ayudante de pastelero y repartidor en empresa familiar.</div>
+	</div>
+	<div class="experiencia">
+		<div class="ano"><b>2008-2010</b></div>
+		<div class="empresa"><b>Comunicaciones Dacuña y Perez, S.L.</b></div>
+		<div class="cargo">Cargo:</div>
+		<div class="datos_cargo">Técnico informático.</div>
+		<div class="proyectos">Función</div>
+		<div class="datos_proyectos">Encargado del mantenimiento informático y del desarrollo y mantenimiento de la web de la empresa realizaa en drupal 6.</div>
+	</div>
+	<div class="experiencia">
+		<div class="ano"><b>2010-hasta ahora</b></div>
+		<div class="empresa"><b>Trabajador por cuenta propia</b></div>
+		<div class="cargo">Cargo:</div>
+		<div class="datos_cargo">Autónomo/freelance</div>
+		<div class="proyectos">Proyectos:</div>
+		<div class="datos_proyectos">
+			<ul>
+				<li>www.gutapc.com Tienda online dedica a la venta de productos electrónicos realizada y mantenida por mi en drupal 6 desde el 2010 hasta ahora.</li>
+				<li>www.mcarquitectos.es Sitio web realizado por mi con drupal 6 durante el 2011 para un estudio de arquitectura.</li>
+				<li>www.eguta.com Página realizada por mi con drupal 7 desde la cual ofrezco mis servicios de diseño web desde el 2012 hasta ahora.</li>
+				<li>www.gruporodriguezcounago.es Sitio realizado por mi en drupal 7 durante el 2012 para un empresa constructora.</li>
+			</ul>			
+		</div>
+	</div>
+	</article>
+</div>
+<div id="formacion_academica">
+	<input id="ac-4" name="accordion-1" type="checkbox" />
+  <label for="ac-4" class="titulo">Formación Académica</label>
+	<article class="ac-large">
+	<div class="formacion">
+		<div class="ano_formacion"><b>1994</b></div>
+		<div class="titulo_formacion"><b>PET</b></div>
+		<div class="datos_formacion">Examén de ingles de la universidad de cambridge realizado en pontevedra (nivel básico).</div>
+	</div>
+	<div class="formacion">
+		<div class="ano_formacion"><b>1994</b></div>
+		<div class="titulo_formacion"><b>Ciclo elemental (Inglés).</b></div>
+		<div class="datos_formacion">Escola oficial de idiomas de pontevedra.</div>
+	</div>
+	<div class="formacion">
+		<div class="ano_formacion"><b>1991-1995</b></div>
+		<div class="titulo_formacion"><b>Titulo bachiller.</b></div>
+		<div class="datos_formacion">IES mendiño. Redondela.</div>
+	</div>
+	<div class="formacion">
+		<div class="ano_formacion"><b>1996-1997</b></div>
+		<div class="titulo_formacion"><b>Estudios Geología.</b></div>
+		<div class="datos_formacion">Universidad de Oviedo.</div>
+	</div>
+	<div class="formacion">
+		<div class="ano_formacion"><b>1999-2004</b></div>
+		<div class="titulo_formacion"><b>Estudios Económicas.</b></div>
+		<div class="datos_formacion">Universidad de vigo.</div>
+	</div>
+	<div class="formacion">
+		<div class="ano_formacion"><b>2006-hasta ahora.</b></div>
+		<div class="titulo_formacion"><b>Ingeniería técnica informática de sistemas.</b></div>
+		<div class="datos_formacion">U.N.E.D<br>En curso, compaginandola con los distintos trabajos que tuve.</div>
+	</div>
+  </article>
+</div>
+<div id="otros_datos">
+	<input id="ac-5" name="accordion-1" type="checkbox" />
+	<label for="ac-5" class="titulo">Otros datos</label>
+	<article class="ac-small">
+		<ul>
+			<li>“Master en diseño web”. 2004</li>
+			<li>“Sair o mercado”. 2012 Curso realizado en la asociacion de jovenes empresarios de vigo, consistente en como llevar una empresa, definicion del plan estrategico, plan de marketing, recursos humanos etc</li>
+			<li>“Curso online de experto en Marketing electrónico para la internalización digital de la PYME” (UNED). 2012</li>
+			<li>Desde 1998 – 2010 compito como semiprofesional en distintos equipos de balonmano, con lo que conozco y estoy acostrumbrado a realizar trabajos en equipo.</li>
+
+		</ul>
+	</article>
+</div>
+</section>
